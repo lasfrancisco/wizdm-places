ce0149f57e675acc381c6b0d7b06d8037830b6d5
lasfrancisco <admin@bizdemon.com>
2019-09-19 13:32:38 +0200

    1 file modified

--- src/app/settings/profile/profile.component.ts
+++ src/app/settings/profile/profile.component.ts
@@ -34,21 +34,7 @@ export class ProfileComponent extends DatabaseDocument<dbUser> {
 
     // Streams the profile photo
     this.photo$ = this.stream().pipe( map( profile => !!profile ? profile.photo : '') );
-
-    this.storage.zone.onMicrotaskEmpty.subscribe( () => {
-
-      console.log('Change detection?');
-
-    });
-
-    //this.storage.ref('lucio.jpg').getDownloadURL().subscribe( url => console.log(url) );
-
-    //this.storage.ref('').list().then( result => console.log(result) );
-
-    this.files = storage.ref('').list();
   }
-
-  readonly files;
 
   // Loads the profile creating a default one when missing
   public load(): Promise<dbUser> {
