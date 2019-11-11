5d89553cad86544e66fa2a3d4bc6c94ec7e41147
lasfrancisco <admin@bizdemon.com>
2019-11-11 11:47:58 +0100

    2 files added, 1 deleted

--- src/app/utils/action-link.service.ts
+++ src/app/utils/action-link.service.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Router, UrlTree, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, UrlTree, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, ParamMap } from '@angular/router';
 import { Observable, of, forkJoin, Subject } from 'rxjs';
 import { switchMap, filter, first, map, tap, zip, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteObserver implements CanActivate {
+export class ActionLink implements CanActivate {
 
   private observers = new Subject<ActivatedRouteSnapshot>();
 
   // Implements single route user authentication guarding
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-
-    console.log(route, state);
     // Pushes the snapshot for observers to react, eventually
     this.observers.next(route);
     // Prevents the real routing
     return false;
   }
 
-  public register(...actions: string[]): Observable<ActivatedRouteSnapshot> {
+  public register(...actions: string[]): Observable<ParamMap> {
 
-    return this.observers.pipe( filter( request => actions.some( action => request.data.action === action )) );
+    return this.observers.pipe( 
+      // Filters the request based on the action code
+      filter( request => actions.some( action => request.data.action === action ) ),
+      // Emits the route's quesy parameters' map
+      map( route => route.queryParamMap ) 
+    );
   }
 }
